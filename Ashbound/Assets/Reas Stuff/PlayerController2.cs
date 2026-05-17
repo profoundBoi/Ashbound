d3f2c68f98aff72c9ca144a1cb51eef9331e4ceb
lxrdflame <tyrique.s.r@gmail.com>
2026-05-17 17:43:08 +0200

    push my stuff

---
+++ Ashbound/Assets/Reas Stuff/PlayerController2.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController3D2 : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 moveInput;
+
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+    private Animator animator;
+
+    [Header("Movement")]
+    public float speed = 5f;
+    public float MaxSpeed;
+    public float jumpForce = 5f;
+
+    //PLayer Animations
+    [Header("Animations")]
+    [SerializeField]
+    private Animator playerAnimations;
+    private bool isJumping;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb.freezeRotation = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    // MOVEMENT
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = new Vector3(1, 0, 0);
+    }
+
+
+    // JUMP
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
+    }
+
+   
+    public void OnGameSelection(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            SceneManager.LoadScene("GameSelect");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = rb.position + transform.TransformDirection(moveInput) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(move);
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+    }
+
+
+}
